ce185b3e57f23b378d38fd47fe093821ea69cb65
ddawng3 <dangnbph24982@fpt.edu.vn>
2024-01-07 14:13:05 +0700

    test

--- src/qlbh/n6/QLBHN6.java
+++ src/qlbh/n6/QLBHN6.java
@@ -14,7 +14,7 @@ public class QLBHN6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
